eb9df2abb7629ccf1cbb159b272d7d71078f6533
emarishkin <ee.marishkin@yandex.ru>
2025-03-27 13:57:21 +0400

    3

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -17,12 +17,12 @@ const [content,setContent] = useState('нажми на кнопку')
             <h3 style={{textAlign:'center',fontSize:30}}>Чем мы отличаемся от других</h3>
            <Inform info={info}/>
             <h3 style={{textAlign:'center',fontSize:30}}>Наш подход к обучению</h3>
-            {/* <div style={{display:'flex',alignItems:'center',gap:'30px'}}>
+            <div style={{display:'flex',alignItems:'center',gap:'30px'}}>
             <Button ClickBro={()=>handleClick('way')} />
             <Button ClickBro={()=>handleClick('easy')}/>
             <Button ClickBro={()=>handleClick('program')}/> 
             </div>
-            <p style={{textAlign:'center',fontSize:20}}>{differences[content]}</p> */}
+            {/* <p style={{textAlign:'center',fontSize:20}}>{differences[content]}</p> */}
             
         </div>
     )
